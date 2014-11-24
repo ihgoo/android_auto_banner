5f713c32e66a8c096559a37ed637a44553924244
kelvinHu <ihgoo2008@gmail.com>
2014-11-25 03:11:10 +0800

    完成viewpager自定义滚动

--- src/com/ihgoo/autobanner/AutoBannerViewPager.java
+++ src/com/ihgoo/autobanner/AutoBannerViewPager.java
@@ -10,6 +10,8 @@ import android.view.MotionEvent;
 
 /**
  * 
+ * 可自动滚动的Banner
+ * 
  * @author <a href="http://www.xunhou.me" target="_blank">Kelvin</a>
  *
  */
@@ -17,7 +19,7 @@ public class AutoBannerViewPager extends ViewPager {
 
 	private final int BANNER_WHAT = 0;
 
-	private Handler handler;
+	private MyHandler handler;
 
 	private boolean isRunning = false;
 
@@ -26,16 +28,18 @@ public class AutoBannerViewPager extends ViewPager {
 	private boolean isStoped;
 
 	private final int LEFT = 0;
-	
+
 	private final int RIGHT = 1;
-	
+
 	private final int START_WHAT = 0;
-	
+
 	private final int STOP_WHAT = 1;
-	
-	private int DEFAULT_ROLL = 1500;
+
+	private int DEFAULT_ROLL_SPEED = 1500;
 
 	private int method = LEFT;
+
+	private boolean smoothScroll = true;
 
 	public AutoBannerViewPager(Context context) {
 		super(context);
@@ -48,24 +52,37 @@ public class AutoBannerViewPager extends ViewPager {
 	}
 
 	private void init() {
-		handler = new Handler();
+		handler = new MyHandler();
 	}
 
 	public void startScroll() {
 		handler.removeCallbacksAndMessages(null);
-		handler.sendEmptyMessageAtTime(START_WHAT, DEFAULT_ROLL);
+		handler.sendEmptyMessageDelayed(START_WHAT, DEFAULT_ROLL_SPEED);
 	}
 
 	public void stopScroll() {
-
+		handler.removeCallbacksAndMessages(null);
+		handler.sendEmptyMessageDelayed(STOP_WHAT, DEFAULT_ROLL_SPEED);
 	}
-	
+
 	/**
-	 * ���ù�������
+	 * 设置滚动的方向
+	 * 
 	 * @param method
+	 *            从左往右 从右往左
 	 */
-	public void setMethod(int method){
+	public void setMethod(int method) {
 		this.method = method;
+	}
+
+	/**
+	 * 设置平滑滚动，从第一个滚动到其他位置 平滑滚动
+	 * 
+	 * @param smoothScroll
+	 *            true为平滑滚动
+	 */
+	public void setSmoothScroll(boolean smoothScroll) {
+		this.smoothScroll = smoothScroll;
 	}
 
 	public void scroll() {
@@ -78,9 +95,9 @@ public class AutoBannerViewPager extends ViewPager {
 
 		int nextItemPostion = (method == LEFT) ? --currentItem : ++currentItem;
 		if (nextItemPostion < 0) {
-			setCurrentItem(currentItem - 1, true);
+			setCurrentItem(totalItem - 1, smoothScroll);
 		} else if (currentItem == totalItem) {
-			setCurrentItem(0, true);
+			setCurrentItem(0, smoothScroll);
 		} else {
 			setCurrentItem(nextItemPostion);
 		}
@@ -92,7 +109,15 @@ public class AutoBannerViewPager extends ViewPager {
 		public void handleMessage(Message msg) {
 			super.handleMessage(msg);
 			switch (msg.what) {
-			case BANNER_WHAT:
+			case START_WHAT:
+				scroll();
+				isRunning = true;
+				handler.sendEmptyMessageDelayed(START_WHAT, DEFAULT_ROLL_SPEED);
+
+				break;
+			case STOP_WHAT:
+				handler.removeCallbacksAndMessages(null);
+				isRunning = false;
 
 				break;
 			default:
